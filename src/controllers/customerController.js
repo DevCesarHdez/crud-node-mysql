434cddf4a73455029f7d73e426112330c04c533e
Cretrok <solosemeocurioesto@gmail.com>
2020-08-12 14:53:28 -0500

    Final??

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -4,7 +4,10 @@ controller.list = (req, res) => {
   req.getConnection((err, conn) => {
     conn.query('SELECT * FROM customer', (err, customers) => {
       if (err) {
-        res.json(err);
+        res.json({
+          error: err,
+          sucess: false
+        });
       }
       //console.log(customers);
       res.render('customers', {
@@ -26,8 +29,34 @@ controller.save = (req, res) => {
   })
 }
 
+controller.edit = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM customer where id = ?', [id], (err, customer) => {
+      res.render('customer_edit', {
+        data: customer[0]
+      });
+    });
+  });
+}
+
+controller.update = (req, res) => {
+  const { id } = req.params;
+  const updateCustomer = req.body;
+  req.getConnection((err, conn) => {
+    conn.query('UPDATE customer SET ? where id = ?', [updateCustomer, id], (err, customer) => {
+      res.redirect('/');
+    });
+  });
+}
+
 controller.delete = (req, res) => {
-  
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query('DELETE FROM customer where id = ?', [id], (err, customer) => {
+      res.redirect('/');
+    });
+  });
 }
 
 module.exports = controller;
