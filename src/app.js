1b2d6bf00b4d5e69402d97ee667984f0f14db1c4
Cretrok <solosemeocurioesto@gmail.com>
2020-08-12 02:35:28 -0500

    Connection to database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+
 const app = express();
 
-app.listen(3000, () => {
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+  host: 'remotemysql.com',
+  user: 'MIWOmbtZpP',
+  password: 'UEVIjDbIt9',
+  port: 3306,
+  database: 'MIWOmbtZpP'
+}, 'single'))
+
+
+app.listen(app.get('port'), () => {
   console.log("server on port 3000");
 })
